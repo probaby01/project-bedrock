4e884f181dda92d8657ff2f6a0c49492fbe2f91b
Paul Adegoke <farmerpisco@gmail.com>
2026-02-09 20:29:31 +0100

    Update EKS module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -55,9 +55,9 @@ resource "aws_eks_node_group" "pb_eks_node_group" {
   subnet_ids      = var.private_subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
-    min_size     = 1
+    desired_size = 4
+    max_size     = 6
+    min_size     = 2
   }
 
   update_config {
