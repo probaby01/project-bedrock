e950a1db5146fc1b2f9149dbb4ecb85d8aa09047
Paul Adegoke <farmerpisco@gmail.com>
2026-02-10 13:38:26 +0100

    Fix bugs in terraform files

--- terraform/modules/monitoring/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudwatch_agent_role_arn" {
-  description = "ARN of the CloudWatch agent IAM role"
-  value       = aws_iam_role.cloudwatch_agent.arn
-}
-
-output "cloudwatch_addon_status" {
-  description = "Status of CloudWatch Observability add-on"
-  value       = aws_eks_addon.cloudwatch_observability.status
-}
